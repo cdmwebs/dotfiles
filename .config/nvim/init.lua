767584e933eb4bd12e48cb719568e5901cf520cf
Chris Moore <chris@knowndecimal.com>
2024-06-09 08:30:44 -0400

    Add gruvbox to neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -723,6 +723,15 @@ require("lazy").setup({
 		end,
 	},
 
+	{
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000,
+		config = true,
+		init = function()
+			vim.cmd("colorscheme gruvbox")
+		end,
+	},
+
 	{ -- You can easily change to a different colorscheme.
 		-- Change the name of the colorscheme plugin below, and then
 		-- change the command in the config to whatever the name of that colorscheme is.
@@ -734,10 +743,10 @@ require("lazy").setup({
 			-- Load the colorscheme here.
 			-- Like many other themes, this one has different styles, and you could load
 			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("tokyonight-night")
+			-- vim.cmd.colorscheme("tokyonight-night")
 
 			-- You can configure highlights by doing something like:
-			vim.cmd.hi("Comment gui=none")
+			-- vim.cmd.hi("Comment gui=none")
 		end,
 	},
 
